59267e502e10491e45559017b19c4195a9736f31
Juan Lucas Rossi <juanlucasrossi@Juans-MacBook-Air.local>
2022-06-11 12:33:10 -0300

    test: ensure validator is called with correct values

--- src/utils/email-validator/email-validator.spec.ts
+++ src/utils/email-validator/email-validator.spec.ts
@@ -15,4 +15,12 @@ describe('EmailValidator', () => {
     const isValid = sut.isValid(email)
     expect(isValid).toBe(true)
   })
+
+  test('Should vall validator with correct value', () => {
+    const sut = new EmailValidatorAdapter()
+    const validatorSpy = jest.spyOn(sut, 'isValid')
+    const email = 'valid_email@gmail.com'
+    sut.isValid(email)
+    expect(validatorSpy).toHaveBeenCalledWith(email)
+  })
 })
